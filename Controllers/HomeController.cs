53fa4e820e4908295286f7005572d1a2133e1659
mightymuddy <84630238+mightymuddy@users.noreply.github.com>
2026-05-29 00:36:42 +0200

    Updated Version v_alpha_0_0_0_1 (#4)
    * Update Dashboard urls
    
    * Upgrading Visuals Adding ASP Identity
    
    * Updating MVC, Designing sapces and colors
    
    * Revert "Updating MVC, Designing sapces and colors"
    
    This reverts commit 4d31c6a4d3df57444fcd36c49e0f3c672277028e.
    
    * Reapply "Updating MVC, Designing sapces and colors"
    
    This reverts commit 5ef8b3399a30116d41d3cf60d99186510f8a6089.
    
    * Switch .NET Version .net 8 to .net 10

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,21 +1,62 @@
-using System.Diagnostics;
+using ClubFlow.Interfaces;
 using ClubFlow.Models;
+using ClubFlow.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
 namespace ClubFlow.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUserRepository _userRepository;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUserRepository userRepository)
         {
             _logger = logger;
+            _userRepository = userRepository;
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult LoginRegister()
+        {
+            return View(new LoginRegisterViewModel());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(LoginRegisterViewModel model, (object success, object errorMessage) value)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("LoginRegister", model);
+            }
+
+            var (success, errorMessage) = await _userRepository.RegisterUserAsync(model.registerLoginModel, model.user);
+            if (!success)
+            {
+                ModelState.AddModelError(string.Empty, errorMessage);
+                return View("LoginRegister", model);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login(LoginRegisterViewModel model)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View("LoginRegister", model);
+            }
+
+            return RedirectToAction("Index");
         }
 
         public IActionResult Dashboard()
---
+++ Models/RegisterLoginModel.cs
@@ -0,0 +1,30 @@
+﻿
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.ComponentModel.DataAnnotations;
+namespace ClubFlow.Models
+{
+    public class RegisterLoginModel
+    {
+
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Passwords do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [Compare("Email", ErrorMessage = "Emails do not match.")]
+        public string ConfirmEmail { get; set; }
+
+    }
+}
